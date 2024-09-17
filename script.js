a33efc0235b9ac4c16bb8a3d182b50d24b9e5d2e
MRX <ayoubaithmad77@gmail.com>
2024-09-17 12:57:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// ==UserScript==
+// @name         linkedin auto follow
+// @namespace    http://tampermonkey.net/
+// @version      2024-09-17
+// @description  linkedin auto follow will automatically follow a list of LinkedIn profiles in just a few easy steps.
+// @author       You
+// @match        https://www.linkedin.com/mynetwork/grow/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+
 (function () {
-    // Select the target node where the changes might happen
+
     var targetNode = document.body;
     var sss = "People you may know based on your recent activity";
     var observerConfig = {childList: true, subtree: true};
     var scrollCountDefault = 2;
     var scrollCount = 0;
-    // Create a MutationObserver instance
+
     var observer = new MutationObserver(function (mutationsList, observer) {
         // console.log("Start searching people container...  ")
         if ($(`.discover-sections-list__item:has(h2:contains(${sss}))`).first().length) {
@@ -47,33 +60,30 @@
         let entitiesModalContent = entitiesModalContainer.find(".artdeco-modal__content");
         let entitiesScroleAble = entitiesModalContainer.find(".scaffold-finite-scroll__content");
         let entities = entitiesScroleAble.find(".discover-fluid-entity-list--item");
-        if (scrollCount < scrollCountDefault) {
-            scrollCount++;
-            entitiesModalContent.scrollTop($(entities.last()).offset().top);
-            for (let i = 0; i < scrollCountDefault; i++) {
-                setTimeout(function () {
-                    entitiesModalContent.scrollTop(0);
-                    scrollToLastEntity(entitiesModalContainer);
-                }, 1000);
-            }
-
-        } else {
-            connect(entitiesModalContainer);
-        }
+        entitiesModalContent.scrollTop($(entities.last()).offset().top);
+        setTimeout(function () {
+            entitiesModalContent.scrollTop(0);
+            scrollToLastEntity(entitiesModalContainer);
+        }, 1000);
+        connect(entitiesModalContainer);
     }
 
     function connect(entitiesModalContainer) {
+        console.log("________________________________________________");
         let entitiesModalContent = entitiesModalContainer.find(".artdeco-modal__content");
         let entitiesScroleAble = entitiesModalContainer.find(".scaffold-finite-scroll__content");
         let entities = entitiesScroleAble.find(".discover-fluid-entity-list--item");
         entities.each(function (index) {
             let entity = $(this);
+
             setTimeout(function () {
                 console.log(
                     entity.find(".discover-person-card__name").text().trim()
                 )
             }, 1000);
+
         });
+        console.log("________________________________________________");
     }
 
 
